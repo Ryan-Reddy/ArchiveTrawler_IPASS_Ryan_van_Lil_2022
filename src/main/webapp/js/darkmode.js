0ab656caf6e9d765de0d1225f9b8143603818875
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-06-22 16:00:50 +0200

    discoball gif hides and shows :D

--- src/main/webapp/js/darkmode.js
+++ src/main/webapp/js/darkmode.js
@@ -1,10 +1,11 @@
-// https://stackoverflow.com/questions/46868253/how-to-enable-dark-mode-in-all-pages
-
 /** Deze functie toggled van darkmode naar light mode en slaat deze keuze op in de localstorage
  *   @Function darkmodeToggle()
  *   @returns void
  */
 function darkmodeToggle() {
+  const discoBallElement = document.getElementById('discoball');
+  discoBallElement.style.display = 'none'; // hide discoball gif
+
   const bodyElement = document.getElementById('body');
   const idAttribute = bodyElement.getAttribute('class');
 
@@ -15,19 +16,6 @@ function darkmodeToggle() {
     // TODO linkDark ook implementeren
   );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
   // Toggle het id van 'body' <=> 'body-dark-mode'
   // //   localStorage.setItem('sheet', sheet);
   // TODO implement localstorage for darkmode so it will toggle sitewide
@@ -37,16 +25,24 @@ function darkmodeToggle() {
 // window.onload = () => swapStyleSheet(localStorage.getItem('body') || 'default.css');
 
 function retroModeToggle() {
+  const discoBallElement = document.getElementById('discoball');
+  discoBallElement.style.display = 'block'; // show discoball gif
+
   const bodyElement = document.getElementById('body');
   const idAttribute = bodyElement.getAttribute('class');
   // TODO retroMode
-  if(!bodyElement.classList.contains('bodyDisco')) {
-  bodyElement.classList.toggle('bodyDisco');
+  if (!bodyElement.classList.contains('bodyDisco')) {
+    bodyElement.classList.toggle('bodyDisco');
   }
+
+  window.onload = () =>
+    bodyElement.classList.add(
+      localStorage.getItem('chosenColorScheme') || 'default.css',
+    );
 
   // // function swapStyleSheet(sheet) {
   // bodyElement.setAttribute(
   //   'class',
   //   idAttribute === 'bodyDisco',
   // )
-};
+}
