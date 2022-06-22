282222746f6841b8a17e36c10fe539be93296e53
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-06-22 11:54:00 +0200

    start retromode

--- src/main/webapp/js/darkmode.js
+++ src/main/webapp/js/darkmode.js
@@ -12,10 +12,49 @@ function darkmodeToggle() {
   bodyElement.setAttribute(
     'class',
     idAttribute === 'bodyDark' ? 'bodyLight' : 'bodyDark',
-  ); // Toggle het id van 'body' <=> 'body-dark-mode'
+    // TODO linkDark ook implementeren
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Toggle het id van 'body' <=> 'body-dark-mode'
   // //   localStorage.setItem('sheet', sheet);
   // TODO implement localstorage for darkmode so it will toggle sitewide
 }
 // https://stackoverflow.com/questions/46868253/how-to-enable-dark-mode-in-all-pages
 
 // window.onload = () => swapStyleSheet(localStorage.getItem('body') || 'default.css');
+
+
+
+
+
+
+
+
+
+
+
+
+
+function retroToggle() {
+  const bodyElement = document.getElementById('body');
+  const idAttribute = bodyElement.getAttribute('class');
+  // TODO retroMode
+
+  // function swapStyleSheet(sheet) {
+  bodyElement.setAttribute(
+    'class',
+    idAttribute === 'bodyDark' ? 'bodyLight' : 'bodyDark',
+  )};
