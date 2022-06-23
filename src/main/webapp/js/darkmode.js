d3336a5f46fe1cd10d0a13088e50690aea797e51
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-06-23 02:26:33 +0200

    theme is held in localstorage !!!

--- src/main/webapp/js/darkmode.js
+++ src/main/webapp/js/darkmode.js
@@ -1,11 +1,28 @@
-const bodyElement = document.getElementById('body');
+/** Dit draait bij het openen pagina: */
+window.onload = () => {
+  LoadPage();
+};
 
-// laad een eventueel opgeslagen thema voorkeur uit localstorage of maak deze bodyDark
-window.onload = () => bodyElement.classList.replace(localStorage.getItem('myArchiveTrawlerTheme')||'bodyDark');
 
-window.onload = () => {if(bodyElement.className === 'bodyDisco') { // als discomode > activeer discobal
-  document.getElementById('discoball').style.display = 'block'; // show discoball gif
-} };
+/** laad een eventueel opgeslagen thema voorkeur uit localstorage of maak deze bodyDark */
+function LoadPage() {
+  const bodyElement = document.getElementById('body');
+  console.log('loading page..')
+  let storedTheme = null;
+
+  try { storedTheme = localStorage.getItem('myArchiveTrawlerTheme'); // probeer thema op te halen uit localStorage
+  } catch {
+    console.log('no theme found in storage, loading default dark mode.')
+  }
+  if (storedTheme != null) {
+    console.log('found theme!' + storedTheme)
+    bodyElement.classList.remove('bodyLight', 'bodyDark', 'bodyDisco');
+    bodyElement.classList.add(storedTheme);
+    if (bodyElement.className === 'bodyDisco') { // als discomode > activeer discobal
+      document.getElementById('discoball').style.display = 'block'; // show discoball gif
+    }
+  }
+}
 
 /** Deze functie toggled van darkmode naar light mode en slaat deze keuze op in de localstorage
  *   @Function darkmodeToggle()
@@ -13,21 +30,27 @@ window.onload = () => {if(bodyElement.className === 'bodyDisco') { // als discom
  */
 function darkmodeToggle() {
   const bodyElement = document.getElementById('body');
-  bodyElement.classList.replace('bodyDisco','bodyDark');
+  bodyElement.classList.replace('bodyDisco', 'bodyDark');
 
   // Toggle het id van 'bodyDark' <=> 'bodyLight'
   bodyElement.classList.toggle('bodyDark');
   bodyElement.classList.toggle('bodyLight');
 
-    localStorage.setItem('myArchiveTrawlerTheme', bodyElement.className);  // sla in localstorage de themakeuze op
+  localStorage.setItem('myArchiveTrawlerTheme', bodyElement.className);  // sla in localstorage de themakeuze op
 
 
   document.getElementById('discoball').style.display = 'none';    // hide discoball gif
 }
 
+/** <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <br />
+ * Deze functie zet de normale dark/lightmode uit
+ * En activeert discoMode kleurenschema, en een discobal!
+ * Ook tovert deze een mooie plaat tevoorschijn om bij te dansen. <br />
+ * <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3
+ */
 function retroModeToggle() {
   const bodyElement = document.getElementById('body');
-  bodyElement.classList.remove('bodyLight','bodyDark');
+  bodyElement.classList.remove('bodyLight', 'bodyDark');
 
   bodyElement.classList.add('bodyDisco');
 
@@ -35,6 +58,7 @@ function retroModeToggle() {
   localStorage.setItem('myArchiveTrawlerTheme', bodyElement.className);  // sla in localstorage de themakeuze op
 
 }
+
 
 
 
