40a0c7644a574896beffc71ec9d2e19ccb07dcf3
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-06-23 05:59:00 +0200

    soundcloud in sights again !

--- src/main/webapp/js/themeManager.js
+++ src/main/webapp/js/themeManager.js
@@ -1,30 +1,44 @@
-/** Dit draait bij het openen pagina: */
-window.onload = () => {
-  LoadPage();
-};
+function setFooter() {
+  console.log('setting up the footergrid');
+  const footer = document.getElementById('footer');
+  footer.classList.add('grid-container');
+  // footer.innerHTML = ''; // start met een verse lege innerHTML
+  footer.innerHTML += '<div class=\'footerLeft\'><iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src="https:\/\/w.soundcloud.com\/player\/?url=https%3A\/\/api.soundcloud.com\/tracks\/51195936&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe> <div style=\"font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;\"> <a href=\"https:\/\/soundcloud.com\/rss\" title=\"RSS Disco\" target=\"_blank\" style=\"color: #cccccc; text-decoration: none;" RSS Disco</a> \u00B7 <a href=\"https:\/\/soundcloud.com\/rss\/rss4\"\r\n title=\"RSS DISCO #004 \/ summer 2012\" target=\"_blank\" style=\"color: #cccccc; text-decoration: none;\">RSS DISCO\r\n #004 \/ summer 2012</a> </div></div>';
+  footer.innerHTML += '<div class=\'footerCenter\' id=\'footerCenter\'><a href="\\/html\\/contact.html\\">Contact</a><br></div>';
+  footer.innerHTML += '<div class=\'footerRight\'><label for=\'darkmodeButton\'>Dark/Light mode:</label><input id=\'darkmodeButton\' onclick=\'darkmodeToggle()\' type="button" value=\'☾☼\'/><br/><label for=\'retroModeButton\'>DISCO mode:</label><input alt=\'retroModeButton\' height=\'25\' id=\'retroModeButton\' onclick=\'retroModeToggle()\'src="/assets/icons/mirror-ball_1faa9.png" type="image" width=\'25\'/></div>';
+
+  const leftFoot = document.getElementById('footerLeft');
+  leftFoot.innerHTML += '<iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\"\r\n    src=\"https:\/\/w.soundcloud.com\/player\/?url=https%3A\/\/api.soundcloud.com\/tracks\/51195936&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"><\/iframe>\r\n<div\r\n    style=\"font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;\">\r\n    <a href=\"https:\/\/soundcloud.com\/rss\" title=\"RSS Disco\" target=\"_blank\"\r\n        style=\"color: #cccccc; text-decoration: none;\">RSS Disco<\/a> \u00B7 <a href=\"https:\/\/soundcloud.com\/rss\/rss4\"\r\n        title=\"RSS DISCO #004 \/ summer 2012\" target=\"_blank\" style=\"color: #cccccc; text-decoration: none;\">RSS DISCO\r\n        #004 \/ summer 2012<\/a><\/div>';
+
+}
 
 /** laad een eventueel opgeslagen thema voorkeur uit localstorage of maak deze bodyDark */
 function LoadPage() {
   const bodyElement = document.getElementById('body');
-  console.log('loading page..')
+  console.log('loading page..');
   let storedTheme = null;
 
-  try { storedTheme = localStorage.getItem('myArchiveTrawlerTheme'); // probeer thema op te halen uit localStorage
+  try {
+    storedTheme = localStorage.getItem('myArchiveTrawlerTheme'); // probeer thema op te halen uit localStorage
   } catch {
-    console.log('no theme found in storage, loading default dark mode.')
+    console.log('no theme found in storage, loading default dark mode.');
   }
   if (storedTheme != null) {
-    console.log('found theme!' + storedTheme)
+    console.log(`found theme!${storedTheme}`);
     bodyElement.classList.remove('bodyLight', 'bodyDark', 'bodyDisco');
     bodyElement.classList.add(storedTheme);
-    if (bodyElement.className === 'bodyDisco') { // als discomode > activeer discobal
+    if (bodyElement.className === 'bodyDisco') {
+      // als discomode > activeer discobal
       document.getElementById('discoball').style.display = 'block'; // show discoball gif
     }
   }
-  const footerCelement = document.getChildById('footer');
-  footerCelement.classList.add('grid-item');
-  footerCelement.innerHTML += 'hello';
+  setFooter();
 }
+
+/** Dit draait bij het openen pagina: */
+window.onload = () => {
+  LoadPage();
+};
 
 /** Deze functie toggled van darkmode naar light mode en slaat deze keuze op in de localstorage
  *   @Function darkmodeToggle()
@@ -38,10 +52,9 @@ function darkmodeToggle() {
   bodyElement.classList.toggle('bodyDark');
   bodyElement.classList.toggle('bodyLight');
 
-  localStorage.setItem('myArchiveTrawlerTheme', bodyElement.className);  // sla in localstorage de themakeuze op
+  localStorage.setItem('myArchiveTrawlerTheme', bodyElement.className); // sla in localstorage de themakeuze op
 
-
-  document.getElementById('discoball').style.display = 'none';    // hide discoball gif
+  document.getElementById('discoball').style.display = 'none'; // hide discoball gif
 }
 
 /** <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <br />
@@ -57,11 +70,5 @@ function retroModeToggle() {
   bodyElement.classList.add('bodyDisco');
 
   document.getElementById('discoball').style.display = 'block'; // show discoball gif
-  localStorage.setItem('myArchiveTrawlerTheme', bodyElement.className);  // sla in localstorage de themakeuze op
-
+  localStorage.setItem('myArchiveTrawlerTheme', bodyElement.className); // sla in localstorage de themakeuze op
 }
-
-
-
-
-
