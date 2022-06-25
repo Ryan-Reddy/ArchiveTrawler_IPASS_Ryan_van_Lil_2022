ad02f6c14c3d220f23f01d70e7bc5350b381cf42
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-06-25 02:46:20 +0200

    ok loading data from rest response mastered ! Lets have some fun..

--- src/main/webapp/js/testRest.js
+++ src/main/webapp/js/testRest.js
@@ -1,7 +1,3 @@
-
-
-
-
 // function testGetAllUsers()  {
 const button = document.querySelector('#getAllUsers');
 
@@ -12,70 +8,50 @@ function getAllUsersButtonFunc() {
     }
   };
 
-  const localhost = 'https://localhost:8080/';
+  const localhost = 'http://localhost:8080/';
 
   // fetch('/restservices/users/', fetchOptions)
-  fetch(localhost+ 'restservices/users/', fetchOptions) // TODO remove outside of normal testing
+  fetch(localhost + 'restservices/users/', fetchOptions) // TODO remove outside of normal testing
     .then((response) => {
       if (!response.ok) {                  // if response =  NOT ok:
         switch (response.status) {
           case 404:
-            console.log("could not find the stuff"); break;
+            console.log("could not find the stuff");
+            break;
           case 401:
-            console.log("unauthorized"); break;
+            console.log("unauthorized");
+            break;
           case 403:
-            console.log("not high enough clearance"); break;
-          default:         throw new Error(response.status.toString());
+            console.log("not high enough clearance");
+            break;
+          default:
+            throw new Error(response.status.toString());
         }
-      }
-      return response.json()              // if response = ok:
+      }             // if response NOT ok
+      return response.json()              // if response IS ok:
         // .then(json => console.log('the response.json: ',json))  // Keep this for troubleshooting
         .then(json => {
-          const array = JSON.stringify(json.map(person => person.naam.string));
-          console.log('array',array);
-          console.log('array.split(',array.split(','));
-          console.log('array.split( [2]',array.split(',')[2]);
+            const array = JSON.stringify(json.map(person => person.naam.string));
+            // console.log('the response.json: ', json)   // do not use the response before assigning it elsewhere
+            console.log('array', array);
+            console.log('array.split(', array.split(','));
+            console.log('array.split( [2]', array.split(',')[2]);
+            const arraySplit = array.split(',');
 
+            arraySplit.forEach(user => document.getElementById('testingspace').innerHTML += '<br>' + user + '<br>')
+            // arraySplit.forEach(user => document.getElementById('testingspace').innerHTML += JSON.stringify(user) + '<br \>')
 
-          } //finally a list of usersnaam // Keep this for troubleshooting
-        // .then(json => {
-        //     let array = JSON.stringify(json.map(person => person.naam.string));
-        //     // let array = json.map(person => person.naam.string);
-        //     for(user in array) {
-        //       document.getElementById('testingspace').innerHTML += user + '<br \>';
-        //     }
-        //   }
+          } //finally a list of usersnaam // Keep this for troubleshooting:
+          // .then(json => {
+          //     let array = JSON.stringify(json.map(person => person.naam.string));
+          //     // let array = json.map(person => person.naam.string);
+          //     for(user in array) {
+          //       document.getElementById('testingspace').innerHTML += user + '<br \>';
+          //     }
+          //   }
         ) //finally a list of usersnaam // Keep this for troubleshooting
     })
 
-
-
-
-
-
-
-    // disassemble array pieces individually - json objects  /** jsonarray */
-    // json object contains -> json array  /** jsonarray */
-    //      - 'email': jsonarray  /** jsonarray */
-    //      - 'naam': jsonarray  /** jsonarray */
-    //      - 'role': jsonarray  /** jsonarray */
-    //      - 'hoeveelheid zoekopdrachten':
-    //            - 'integral' :true;
-    // for (i in jsonResponse)   TODO still trying to display on screen
-    // {
-    //   for (j in jsonResponse[i])
-    //   {
-    //     x = jsonResponse[i][j];
-    //     console.log(x);
-    //     console.table(x);
-    // }
-    // }
-    // } else if (response.status == 404) {
-    //   console.log("could not find the stuff");
-    // } else if (response.status == 401) {
-    //   console.log("unauthorized")
-    // }
-    // }
     .catch(error => {
       console.log(error);
     })
