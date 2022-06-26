f622f2b1de06a120f46bc5d2c4d9131b5378f5ff
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-06-26 22:47:33 +0200

    amsterdam archive search function expanded

--- src/main/webapp/js/zoeken.js
+++ src/main/webapp/js/zoeken.js
@@ -3,8 +3,9 @@ function zoekButtonClicked() {
   const voornaam = document.getElementById('voornaam').value;
   const achternaam = document.getElementById('achternaam').value;
   const tussenvoegsel = document.getElementById('tussenvoegsel').value;
-  const jaarVan = document.getElementById('jaarVan').value;
-  const jaarTot = document.getElementById('jaarTot').value;
+
+  let jaarVan = document.getElementById('jaarVan').value;
+  let jaarTot = document.getElementById('jaarTot').value;
 
   const dublinArchive = document.getElementById('archiveDublin');
   const amsterdamArchive = document.getElementById('archiveAmsterdam');
@@ -22,12 +23,15 @@ function zoekButtonClicked() {
   console.log(amsterdamArchive);
 
   if (amsterdamArchive.checked === true) {
+    if (jaarVan === '') {
+      jaarVan = 1400;
+    }
+    if (jaarTot === '') {
+      jaarTot = 2000;
+    }
     window.open(
-      `https://archief.amsterdam/indexen/persons?sa=%7B%22person_1%22:%7B%22q%22:%22${keywords}%22,%22search_t_geslachtsnaam%22:%22${achternaam}%22,%22search_t_tussenvoegsel%22:%22${tussenvoegsel}%22,%22search_t_voornaam%22:%22${voornaam}%22%7D%7D`,
+      `${archiefAmsBaseURL}f=%7B%22search_i_datum%22:%7B%22v%22:%5B%22${jaarVan}0000%22,%22${jaarTot}9999%22%5D,%22d%22:%22${jaarVan}%20-%20${jaarTot}%22%7D%7D&sa=%7B%22person_1%22:%7B%22q%22:%22${keywords}%22,%22search_t_geslachtsnaam%22:%22${achternaam}%22,%22search_t_tussenvoegsel%22:%22${tussenvoegsel}%22,%22search_t_voornaam%22:%22${voornaam}%22%7D%7D`,
       '_blank',
     );
   }
 }
-// document
-//   .getElementById('#searchButton')
-//   .addEventListener('click', zoekButtonClicked());
