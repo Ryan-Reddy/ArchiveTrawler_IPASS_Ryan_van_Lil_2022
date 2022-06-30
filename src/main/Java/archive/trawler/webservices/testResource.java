a6998a06489a5f091b54bc70e001f004ad4352a1
Ryan Reddy <90551370+Ryan-Reddy@users.noreply.github.com>
2022-06-30 13:59:38 +0200

    find single user works, now to implement on changeacc info page. Also an attempt at statuscheck fix

--- src/main/Java/archive/trawler/webservices/testResource.java
+++ src/main/Java/archive/trawler/webservices/testResource.java
@@ -6,13 +6,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Path("status")
 public class testResource {
 
     /** functie checkAPIstatus
      * check dat de rest API werkt
      */
     @GET
-    @Path("status")
     @Produces(MediaType.APPLICATION_JSON)
     public Response checkAPIstatus() {
         return Response.ok("works").build();
--- src/main/webapp/js/findUser.js
+++ src/main/webapp/js/findUser.js
@@ -2,8 +2,8 @@ async function fetchSingleUser() {
   console.log('fetchSingleUser() (jwt.thisUser)')
   const jsonRequestBody = {};
 
-  const formData = new FormData(document.querySelector('#emailToSearchForm'));
-  formData.forEach((value, key) => (jsonRequestBody[key] = value));
+  // const formData = new FormData(document.querySelector('#emailToSearchForm'));
+  // formData.forEach((value, key) => (jsonRequestBody[key] = value));
 
   const fetchOptions = {
     method: 'POST',
@@ -14,11 +14,15 @@ async function fetchSingleUser() {
       'Content-Type': 'application/json',
     },
   };
+  const testingspaceSpan = document.getElementById('testingspace');
   await fetch('/restservices/users/getAccount/', fetchOptions) // een POST naar dit adres maakt een nieuw acc.
     .then(async (response) => {
       if (response.status === 200) { // er is een account gevonden !
         let myJson = await response.json();
         console.log(myJson);
+        testingspaceSpan.innerHTML += 'naam:  ' + myJson.naam;
+        testingspaceSpan.innerHTML += '<br>email: ' + myJson.email;
+        testingspaceSpan.innerHTML += '<br>rol:   ' + myJson.role;
       }
       if (response.status === 204) { //no content
         console.log('geen content gevonden voor die input');
