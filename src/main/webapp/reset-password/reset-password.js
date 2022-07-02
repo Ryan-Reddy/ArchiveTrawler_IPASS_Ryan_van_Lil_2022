e31de7ac84c1a6a412ecb498b4bcaef41a7c9c9f
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-07-02 11:01:26 +0200

    worked till accountinfo checking style on site

--- src/main/webapp/reset-password/reset-password.js
+++ src/main/webapp/reset-password/reset-password.js
@@ -2,41 +2,62 @@ async function wijzigWachtwoord() {
   const feedbackspan = document.getElementById('postresponse');
   const ww1 = document.getElementById('password').value;
   const ww2 = document.getElementById('password2').value;
-  if(ww1.length < 6 || ww2.length < 6) {
-    console.log("wachtwoord te kort")
+  if (ww1.length < 6 || ww2.length < 6) {
+    console.log('wachtwoord te kort');
     feedbackspan.innerHTML = '<p color=red>Uw wachtwoord is te kort!</p>';
   }
   if (ww1 != ww2) {
-    console.log("wachtwoorden verschillen")
+    console.log('wachtwoorden verschillen');
     feedbackspan.innerHTML = '<p>Uw wachtwoorden komen niet overeen, controleer nogmaals</p>';
   }
-  if (ww1 === ww2 && ww1.length >= 6 && ww2.length >= 6)  {
+  if (ww1 === ww2 && ww1.length >= 6 && ww2.length >= 6) {
     const jsonRequestBody = {};
-    let formData = new FormData(document.querySelector('#wijzigWachtwoord'));
+    const formData = new FormData(document.querySelector('#wijzigWachtwoord'));
     formData.forEach((value, key) => (jsonRequestBody[key] = value));
-    console.log('with data: ' + jsonRequestBody.toString());
+    console.log(`with data: ${jsonRequestBody.toString()}`);
 
     const fetchOptions = {
-      method: 'PATCH', body: JSON.stringify(jsonRequestBody), headers: {
+      method: 'PATCH',
+      body: JSON.stringify(jsonRequestBody),
+      headers: {
         Authorization: `Bearer ${sessionStorage.JWT}`, Accept: 'application/json', 'Content-Type': 'application/json',
       },
     };
     await fetch(`${localhost}restservices/account-reset/wijzig-wachtwoord/`, fetchOptions) // een POST naar dit adres maakt een nieuw acc.
       .then(async (response) => {
         if (response.ok) {
-          const feedback = "Accountinfo wijzigen is gelukt!";
+          const feedback = 'Accountinfo wijzigen is gelukt!';
           feedbackspan.innerText = feedback;
           return feedback;
-        } else {
-          feedbackspan.innerText = 'bent u ingelogd?';
-          throw 'er ging iets mis';
-        } // if !200 there will be no body
+        }
+        feedbackspan.innerText = 'bent u ingelogd?';
+        throw 'er ging iets mis';
+        // if !200 there will be no body
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       });
+  } else {
+    console.log('whoops ... have you tried turning it on and off again?');
   }
-  else {
-    console.log('whoops ... have you tried turning it on and off again?')
-  }
+}
+
+/** Deze functie togglet de zichtbaarheid van het wachtwoord heen en weer
+ *   @Function wwZichtbaarToggle()
+ *   @returns void
+ */
+function wwZichtbaarToggle() {
+  const passwordElement = document.getElementById('password');
+  const passwordElement2 = document.getElementById('password2');
+  const typeAttribute = passwordElement.getAttribute('type');
+
+  // Switch het type van 'password' <=> 'text' en vice versa
+  passwordElement.setAttribute(
+    'type',
+    typeAttribute === 'password' ? 'text' : 'password',
+  );
+  passwordElement2.setAttribute(
+    'type',
+    typeAttribute === 'password' ? 'text' : 'password',
+  );
 }
