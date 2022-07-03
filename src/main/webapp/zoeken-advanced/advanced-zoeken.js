10be9662dad4aefd3996728cc81989f3d445c396
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-07-03 17:18:09 +0200

    search page looking goood

--- src/main/webapp/zoeken-advanced/advanced-zoeken.js
+++ src/main/webapp/zoeken-advanced/advanced-zoeken.js
@@ -20,7 +20,7 @@ function advancedSearchButtonClicked() {
   fetch(`${localhost}restservices/search-advanced-service/`, fetchOptions)
     .then(async (response) => {
       if (response.status === 200) {
-        // er is een account gevonden !
+        feedbackSpan.innerHTML = "<hr><br><br>Zoekresultaten:<br>"
         const myJson = await response.json(); // return the search results incl changelog
         console.log(myJson);
         const myPrettyJsonString = JSON.stringify(myJson, null, 2);
@@ -29,14 +29,29 @@ function advancedSearchButtonClicked() {
         //   const value = obj[key];
         //   document.write(`<br> - ${key}: ${value}`);
         // }
-        searchDataTable.innerHTML = '';
+        searchDataTable.innerHTML = ''; // reset de tabel data
         const JsonPersonsAsArrayArchAms = JSON.parse(myPrettyJsonString).person;
         // const JsonPersonsAsArrayOpenArch = JSON.parse(myPrettyJsonString);
+        searchDataTable.innerHTML += ''
+        + ` <tr><th id="header">Voornaam</th> `
+        + ` <th id="header">Achternaam</th> `
+        + ` <th id="header">Rol</th> `
+        + ` <th id="header">Datum</th> `
+        + ` <th id="header">Plaats</th> `
+        + ` <th id="header">Geboorte</th> `
+        + ` <th id="header">Gewijzigd</th> `
+        + ` <th id="header">Link</th></tr>`;
 
         JsonPersonsAsArrayArchAms.forEach((element) => { // for amsterdam archive
-          searchDataTable.innerHTML += `<tr><th>${element.metadata.voornaam}</th> `
+          searchDataTable.innerHTML += ''
+          + ` <tr><th>${element.metadata.voornaam}</th> `
           + ` <th>${element.metadata.achternaam}</th> `
-          + ` <th><a href="https://webservices.picturae.pro/genealogy/person/${element.id}">Link</a></th></tr>`;
+          + ` <th>${element.metadata.deed_type_title}</th> `
+          + ` <th>${element.metadata.datum}</th> `
+          + `<th>${element.metadata.register_gemeente}</th> `
+          + ` <th>${element.metadata.datum_geboorte}</th> `
+          + ` <th>${element.metadata.modified_time}</th> `
+          + ` <th><a href="https://archief.amsterdam/indexen/deeds/${element.deed_id}?person=${element.id}">Link</a></th></tr>`;
         });
       }
       if (response.status === 403) {
